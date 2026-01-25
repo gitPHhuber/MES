81d0a4c1e8e9797296731c605bfa6ac769d9277d
gitPHhuber <gptusers.user1@gmail.com>
2026-01-25 21:05:56 +0300

    Fix production model table names

--- MES-Kryptonit-Server-master/models/definitions/Structure.js
+++ MES-Kryptonit-Server-master/models/definitions/Structure.js
@@ -1,21 +1,37 @@
 const sequelize = require("../../db");
 const { DataTypes } = require("sequelize");
 
-const Section = sequelize.define("production_section", {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  title: { type: DataTypes.STRING, allowNull: false, unique: true },
-  description: { type: DataTypes.STRING },
-});
-
-const Team = sequelize.define("production_team", {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  title: { type: DataTypes.STRING, allowNull: false },
-  sectionId: {
-    type: DataTypes.INTEGER,
-    allowNull: true,
-    field: "productionSectionId",
+const Section = sequelize.define(
+  "production_section",
+  {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: { type: DataTypes.STRING, allowNull: false, unique: true },
+    description: { type: DataTypes.STRING },
   },
-});
+  {
+    tableName: "production_sections",
+    underscored: true,
+    freezeTableName: true,
+  }
+);
+
+const Team = sequelize.define(
+  "production_team",
+  {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: { type: DataTypes.STRING, allowNull: false },
+    sectionId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      field: "productionSectionId",
+    },
+  },
+  {
+    tableName: "production_teams",
+    underscored: true,
+    freezeTableName: true,
+  }
+);
 
 module.exports = {
   Section,
